46a417a11ef2567d0d51dfac6416730bf95867f2
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2024-12-26 19:17:24 +0100

    More gitignore

--- src/components/home.rs
+++
@@ -1,6 +0,0 @@
-use dioxus::prelude::*;
-
-#[component]
-pub fn Home() -> Element {
-    rsx! { h1 { "Welcome to the Homepage!" } }
-}
--- src/components/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod navbar;
-pub mod home;
-pub mod login;
-pub mod page_not_found;
